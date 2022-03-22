e95440c0602b761e3f05ef24678cf2d8160b1a45
Kacper Kafara <kacperkafara@gmail.com>
2022-03-22 20:30:34 +0100

    chore: Update deps

--- src/bot.js
+++ src/bot.js
@@ -1,37 +1,15 @@
-import Discord from "discord.io";
-import logger from 'winston';
+import { Client, Intents } from 'discord.js'
 
-logger.remove(logger.transports.Console)
-logger.add(new logger.transports.Console, { colorize: true })
-logger.level = 'debug'
-
-const authorizationInfo = {
-	token: process.env.BOT_TOKEN,
+const authInfo = {
+	token: process.env.BOT_TOKEN
 }
 
-const bot = new Discord.Client({
-	token: authorizationInfo.token,
-	autorun: true,
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS]
 })
 
-bot.on('ready', function(event) {
-	logger.info("Connectd & Logged in as: " + bot.username + " - " + bot.id)
+client.once('ready', () => {
+	console.log("Bot is ready!")
 })
 
-bot.on('message', function(user, userID, channelID, message, event) {
-	if (message.startsWith('!', 0)) {
-		const command = message.slice(1, message.length);
-		logger.info("Received command: " + command)
-
-		switch (command) {
-			case 'ping':
-				bot.sendMessage({
-					to: channelID,
-					message: "Pong!"
-				})
-				break;
-			default:
-				logger.info("Invalid command")
-		}
-	}
-})
+client.login(authInfo.token)
