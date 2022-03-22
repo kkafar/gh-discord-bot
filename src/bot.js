69ceb355ed73d3e3df78f0250ea42f0e5b07e2a5
Kacper Kafara <kacperkafara@gmail.com>
2022-03-22 21:14:39 +0100

    feat: Connect with github api

--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,30 @@
 import { Client, Intents } from 'discord.js'
+import axios from 'axios'
 
 const authInfo = {
-	token: process.env.BOT_TOKEN
+	discordToken: process.env.BOT_DISCORD_TOKEN,
+	githubToken: process.env.BOT_GITHUB_TOKEN
 }
 
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS]
 })
 
-client.once('ready', () => {
+client.once('ready', async () => {
 	console.log("Bot is ready!")
 })
 
-client.login(authInfo.token)
+client.login(authInfo.discordToken)
+
+const config = {
+	headers: {
+		authorization: "token " + authInfo.githubToken
+	}
+}
+
+// const url = 'https://api.github.com/orgs/RPG-IO/repos'
+const url = 'https://api.github.com/repos/RPG-IO/rpg-io/pulls'
+
+const response = await axios.get(url, config)
+
+console.log(await response)
